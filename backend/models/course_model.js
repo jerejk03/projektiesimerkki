c92fc0c2963a95e58d36adfacd80d698eb8d385a
Jere Kaasalainen <jere.j.kaasalainen@gmail.com>
2025-03-11 12:51:19 +0200

    course (kesken, erroreita)

--- backend/models/course_model.js
+++ backend/models/course_model.js
@@ -8,7 +8,12 @@ const course={
         return db.query('SELECT * FROM course WHERE idcourse=?',[id],callback);
     },
     add(newCourse,callback){
-        return db.query('INSERT INTO course(course_name, credit_points) VALUES(??)');
+        return db.query('INSERT INTO course(course_name, credit_points) VALUES(?,?)',[newCourse.course_name, newCourse.credit_points],callback);
+    },
+    update(id,newData,callback){
+        return db.query('UPDATE course SET course_name=?, credit_points WHERE idcourse=?',
+            [newData.course_name, newData.credit_points, id],callback
+        );
     }
 }
 
--- backend/routes/course.js
+++ backend/routes/course.js
@@ -24,5 +24,38 @@ router.get('/:id',function(request, response){
     })
 });
 
+router.post('/',function(request, response){
+    course.add(request.body, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else{
+            response.json(result.affectedRows);
+        }
+    })
+});
+
+router.put('/:id',function(request, response){
+    course.update(request.params.id, request.body, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else{
+            response.json(result.affectedRows);
+        }
+    })
+});
+
+router.delete('/:id',function(){
+    course.delete(request.params.id, function(err, result){
+        if(err){
+            response.json(err);
+        }
+        else{
+            response.json(result.affectedRows);
+        }
+    })
+});
+
 
 module.exports=router;
